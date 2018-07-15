e651007db76c7868b7d237134954c5a5b619b869
RinniSwift <rinradaswift@gmail.com>
2018-07-15 14:53:18 -0700

    UI-Currency-Exchange-Calculator

---
+++ Supporting Files/UIColor+TC.swift
@@ -0,0 +1,23 @@
+//
+//  UIColor+TC.swift
+//  CurrencyExchageCalculator
+//
+//  Created by Rinni Swift on 7/8/18.
+//  Copyright © 2018 Rinni Swift. All rights reserved.
+//
+
+import Foundation
+
+import UIKit.UIColor
+
+extension UIColor {
+    static var tcWhite: UIColor{
+        return UIColor(named: "tcWhite")!
+    }
+    static var tcOffWhite: UIColor {
+        return UIColor(named: "tcOffWhite")!
+    }
+    static var tcSeafoamGreen: UIColor {
+        return UIColor(named: "tcSeafoamGreen")!
+    }
+}
